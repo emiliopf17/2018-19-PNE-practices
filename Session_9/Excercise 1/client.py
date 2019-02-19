44e2f0af81c5b05af258d402ed6ff30a2013a7f6
emiliopf17 <46930580+emiliopf17@users.noreply.github.com>
2019-02-19 15:55:26 +0100

    Add files via upload

---
+++ Session_9/Excercise 1/client.py
@@ -0,0 +1,23 @@
+import socket
+import termcolor
+import sys
+
+# SERVER IP, PORT
+IP = "127.0.0.1"
+PORT = 8084
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# establish the connection to the Server (IP, PORT)
+s.connect((IP, PORT))
+
+msg = input ("> ")
+
+# Send the request message to the server
+s.send(str.encode(msg))
+
+# Receive the servers respoinse
+response = s.recv(2048).decode()
+
+# Print the server's response
+print("Response: {}".format(response))
