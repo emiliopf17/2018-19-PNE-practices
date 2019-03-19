3af4a62fe7b93ea47f33763650de2c93f099fdb5
emiliopf17 <46930580+emiliopf17@users.noreply.github.com>
2019-03-19 17:52:19 +0100

    Add files via upload

---
+++ Session-17/Excercise_1/ex1.py
@@ -0,0 +1,35 @@
+import json
+import termcolor
+
+
+f = open("person_ex1.json", 'r')
+
+
+persons = json.load(f)
+print("Total people:",len(persons["Persons"]))
+people_list = persons["Persons"]
+for j, person in enumerate (people_list):
+
+    print()
+    termcolor.cprint("Name: ", 'green', end="")
+    print(person['Firstname'], person['Lastname'])
+
+    termcolor.cprint("Age: ", 'green', end="")
+    print(person['age'])
+
+
+    phoneNumbers = person['phoneNumber']
+
+
+    termcolor.cprint("Phone numbers: ", 'green', end='')
+    print(len(phoneNumbers))
+
+    # Print all the numbers
+    for i, num in enumerate(phoneNumbers):
+        termcolor.cprint("  Phone {}:".format(i), 'blue')
+
+
+        termcolor.cprint("    Type: ", 'red', end='')
+        print(num['type'])
+        termcolor.cprint("    Number: ", 'red', end='')
+        print(num['number'])
